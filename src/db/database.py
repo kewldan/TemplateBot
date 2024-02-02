faa25208074d0394e6fcda392aab9b4d101351ed
kewldan <kewldanil1@gmail.com>
2024-02-02 17:21:12 +0300

    Beanie migration

--- src/db/database.py
+++ src/db/database.py
@@ -2,23 +2,28 @@ import math
 import time
 from typing import Optional
 
-import motor.motor_asyncio
+from beanie import Document, init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import config
-from db.types.user import User
 
-client = motor.motor_asyncio.AsyncIOMotorClient(config['bot']['mongo'])
-database = client[config['bot']['database']]
-users = database['users']
+
+class User(Document):
+    id: int
+    username: Optional[str] = None
+    joined: int
+
+
+client = AsyncIOMotorClient(config['bot']['mongo'])
+
+
+async def connect():
+    await init_beanie(database=client.db_name, document_models=[User])
 
 
 async def get_user(user_id: int, username: Optional[str]) -> User:
-    user = await users.find_one({'id': user_id})
+    user = await User.find_one(User.id == user_id)
     if not user:
-        await users.insert_one({
-            'id': user_id,
-            'username': username,
-            'joined': math.floor(time.time())
-        })
-        user = await users.find_one({'id': user_id})
-    return User(**user)
+        user = User(id=user_id, username=username, joined=math.floor(time.time()))
+        await user.insert()
+    return user
--- src/db/types/user.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    id: int
-    username: Optional[str]
-    joined: int
